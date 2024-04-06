91fb36d8a0671d3aaf0059451480c8d73e9af51d
Yui Semba <65758369+semba-yui@users.noreply.github.com>
2024-04-06 17:08:16 +0900

    refactor: apply format

--- src/DemoComp.DemoGrpcServer/Program.cs
+++ src/DemoComp.DemoGrpcServer/Program.cs
@@ -28,7 +28,11 @@ app.MapGet("/",
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 var env = app.Environment;
-if (env.IsDevelopment()) app.MapGrpcReflectionService();
+
+if (env.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 
 app.Run();
 
